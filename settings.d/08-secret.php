c9c129238dd4543dd08ed1bc58e2ad91e501c2ee
Chunliang Lyu <hi@chunlianglyu.com>
2016-05-04 21:05:42 +0800

    Copy settings to the image during build

--- settings.d/08-secret.php
+++ settings.d/08-secret.php
@@ -2,8 +2,8 @@
 
 # NOTE: make sure you change the variables in this file!
 
-$wgSecretKey = "ee0efe6c8b4bc1ee5ccd906ad783aeb20115f061a3f9d85e6850612104920701";
+$wgSecretKey = getenv("WG_SECRET_KEY");
 
 # Site upgrade key. Must be set to a string (default provided) to turn on the
 # web installer while LocalSettings.php is in place
-$wgUpgradeKey = "08882e35d74f30cf";
+$wgUpgradeKey = getenv("WG_UPGRADE_KEY");
